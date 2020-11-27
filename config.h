882f494c8171038258b0d1b88cff9bc386d562d0
Jensun Ravichandran <jjensun@gmail.com>
2020-11-27 14:35:00 +0100

    Reorder blocks

--- config.h
+++ config.h
@@ -14,21 +14,21 @@ static const Block blocks[] = {
 	/* {"",	"price eth Ethereum 🍸",	9000,	23}, */
 	/* {"",	"price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"torrent",	20,	7}, */
+	/* {"",	"moonphase",	18000,	17}, */
 	{"",	"memory",	10,	14},
 	{"",	"cpu",		10,	18},
-	/* {"",	"moonphase",	18000,	17}, */
 	{"",	"forecast",	18000,	5},
 	{"",	"mailbox",	180,	12},
-	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
+	{"",	"nettraf",	1,	16},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	{"",	"clock",	60,	1},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = "\0";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
